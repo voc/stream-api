9f6917ddfbaaadcee622f12e8586f0d29dce048b
Anton Schubert <ischluff@mailbox.org>
2021-11-01 00:43:34 +0100

    update upload scripts

--- scripts/upload_dash.sh
+++ scripts/upload_dash.sh
@@ -9,4 +9,4 @@ ffmpeg -hide_banner -i rtmp://loopinglouie.fem.tu-ilmenau.de/rtmp/istuff_live -c
     -window_size 10 -extra_window_size 2 \
     -media_seg_name 'chunk-stream$RepresentationID$-$Number$.$ext$' \
     -auth_type basic \
-    -method PUT "http://${user}:${pass}@localhost:8080/live/out.mpd"
+    -method PUT "http://${user}:${pass}@localhost:8080/dash/live/out.mpd"
--- scripts/upload_hls.sh
+++ scripts/upload_hls.sh
@@ -6,4 +6,4 @@ ffmpeg -hide_banner -i rtmp://loopinglouie.fem.tu-ilmenau.de/rtmp/istuff_live -c
     -f hls \
     -http_persistent 1 -timeout 2 \
     -hls_flags delete_segments -auth_type basic \
-    -method PUT "http://${user}:${pass}@localhost:8080/live/out.m3u8"
+    -method PUT "http://${user}:${pass}@localhost:8080/hls/live/out.m3u8"
