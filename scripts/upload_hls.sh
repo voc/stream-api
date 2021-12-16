0a267e97fab54b021d6adea10e5f15ee4bcb7b8c
Anton Schubert <ischluff@mailbox.org>
2021-12-16 13:40:39 +0100

    update upload test script

--- scripts/upload_hls.sh
+++ scripts/upload_hls.sh
@@ -5,5 +5,5 @@ pass=$2
 ffmpeg -hide_banner -i rtmp://loopinglouie.fem.tu-ilmenau.de/rtmp/istuff_live -c copy \
     -f hls \
     -http_persistent 1 -timeout 2 \
-    -hls_flags delete_segments -auth_type basic \
-    -method PUT "http://${user}:${pass}@localhost:8080/hls/live/out.m3u8"
+    -hls_flags delete_segments \
+    -method PUT "http://localhost:8080/hls/live/out.m3u8"
