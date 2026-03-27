c201b0dfc518840b5ee36d924ab63616f920e3b6
Anton Schubert <ischluff@mailbox.org>
2026-03-28 00:48:59 +0100

    update upload test script

--- scripts/upload_hls.sh
+++ scripts/upload_hls.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # uploading ffmpeg client
-user=$1
-pass=$2
 ffmpeg -hide_banner -i rtmp://loopinglouie.fem.tu-ilmenau.de/rtmp/istuff_live -c copy \
+    -map 0:a -map 0:v -map 0:a -map 0:v \
+    -b:v 2000k \
     -f hls \
-    -http_persistent 1 -timeout 2 \
+    -http_persistent 1 \
     -hls_flags delete_segments \
-    -method PUT "http://localhost:8080/hls/live/out.m3u8"
+    -master_pl_name native_hd.m3u8 \
+    -var_stream_map "v:0,a:0 v:1,a:1" \
+    -master_pl_publish_rate 10 \
+    -method PUT 'http://localhost:8080/hls/live/out_%v.m3u8'
